b361ae5ecd4f5d45b146a009d6847b2123e26dff
Artem <a.abovin@gmail.com>
2020-02-22 15:16:34 -0800

    Updates

---
+++ precache-manifest.ecbff6e2675e1f8c6db50980653af0d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddc5ce1a74a242bd40efeddccb36c6c",
+    "url": "/capuashop/index.html"
+  },
+  {
+    "revision": "ea3459a9706a7f6b95a1",
+    "url": "/capuashop/static/css/main.094a8062.chunk.css"
+  },
+  {
+    "revision": "cb350590bd3953a04c88",
+    "url": "/capuashop/static/js/2.203f32e4.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/capuashop/static/js/2.203f32e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea3459a9706a7f6b95a1",
+    "url": "/capuashop/static/js/main.37dbda59.chunk.js"
+  },
+  {
+    "revision": "996b34993f3e2dcf1ba5",
+    "url": "/capuashop/static/js/runtime-main.dd56c402.js"
+  },
+  {
+    "revision": "9b7988fe6eb4072160150836396ac0f3",
+    "url": "/capuashop/static/media/banner3.9b7988fe.jpg"
+  },
+  {
+    "revision": "559930d4ac88eec062856d5d9900a695",
+    "url": "/capuashop/static/media/choose.559930d4.jpg"
+  },
+  {
+    "revision": "b44016dfcbac9f340e66470c042889bf",
+    "url": "/capuashop/static/media/favicon.b44016df.png"
+  },
+  {
+    "revision": "a8c948b724f574b8b4348c97205cc901",
+    "url": "/capuashop/static/media/follow.a8c948b7.jpg"
+  },
+  {
+    "revision": "78bd3e5cd44f5c4529747405a5d72117",
+    "url": "/capuashop/static/media/make.78bd3e5c.jpg"
+  },
+  {
+    "revision": "9e0c88338db20412591bfb6e4c048602",
+    "url": "/capuashop/static/media/test.9e0c8833.jpg"
+  }
+]);
